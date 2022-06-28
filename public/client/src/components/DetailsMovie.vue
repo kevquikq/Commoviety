66f3df8d50732b78a0113f27ae5ae473704fd130
Uriel1621 <uriel.fischman@gmail.com>
2022-06-28 16:22:46 -0300

    Funcionalidad de puntuar

--- public/client/src/components/DetailsMovie.vue
+++ public/client/src/components/DetailsMovie.vue
@@ -5,24 +5,62 @@
       <div class="jumbotron">
       <div class="row">
         <div class="card col-4" style="width:100px">
-          <img :src="traerUrl(poster_path)" alt="">
+          <img :src="traerUrl(getPeliculas[index].image)" alt="">
           <br>
-          <p> Calificacion: </p>
-          <p> <svg style="margin-bottom: 10px;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+          <p> Score: </p>
+          <p v-if="getPeliculas[index].averageScore"> <svg style="margin-bottom: 10px;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
           <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-           </svg> {{vote_average}}/10</p>
+           </svg> {{getPeliculas[index].averageScore}}/10 </p>
+          <p v-else> N/A </p>
         </div>
         <div class="col">
-        <h1>{{title}}</h1>
-        <br>
-        <p class="ml-5">{{overview}}</p>
+          <h1>{{getPeliculas[index].name}}</h1>
+          <br>
+          <p class="ml-5">{{getPeliculas[index].description}}</p>
+          <br>
+          <div v-show="getUsuarioActual">
+            <div v-if ="!puntajeEnviado">
+              <vue-form :state="formstate" @submit.prevent="enviarPuntaje()">
+                <validate tag="div">
+                  <label for="puntaje">Puntaje:</label>
+                  <input
+                    type="number"
+                    id="puntaje"
+                    v-model.number="formData.puntaje"
+                    name="puntaje"
+                    autocomplete="off"
+                    class="form-control"
+                    required
+                    :min="minPuntaje"
+                    :max="maxPuntaje"
+                  />
+
+                  <field-messages name="puntaje" show="$dirty">
+                    <div slot="required" class="alert alert-danger mt-1">
+                      El campo es requerido
+                    </div>
+                    <div slot="min" class="alert alert-danger mt-1">
+                      El puntaje minimo es 1
+                    </div>
+                    <div slot="max" class="alert alert-danger mt-1">
+                      El puntaje maximo es 10
+                    </div>
+                  </field-messages>
+                </validate>
+                <br>
+                <button class="btn btn-danger my-3 col-12" :disabled="formstate.$invalid" type="submit">Enviar puntaje</button>
+              </vue-form>
+            </div>
+            <div v-else class="alert alert-success">
+              Se envio el puntaje
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <button @click="showList"  class="btn btn-danger col-12" 
     style="background:#F48660">Volver a la lista</button>
     </div>
-    
   </section>
 
 </template>
@@ -30,12 +68,19 @@
 <script >
 export default {
   name: "src-components-show-movie",
-  props: ["title","poster_path","overview","vote_average"],
+  props: ["index"],
   mounted() {
-
+    this.actualizarPeliculas()
   },
   data() {
-    return {};
+    return {
+        formstate: {},
+        formData: this.getInitialData(),
+        minPuntaje: 1,
+        maxPuntaje: 10,
+        puntajeEnviado: false,
+        urlPuntuar: 'http://localhost:4444/scoreUser'
+      }
   },
   methods: {
     showList() {
@@ -43,9 +88,33 @@ export default {
           path: '/searchMovies'
       })
     },
+    getInitialData() {
+        return {
+          puntaje: "",
+        }
+    },
     traerUrl(path) {
       return "https://image.tmdb.org/t/p/original" + path;
     },
+    async enviarPuntaje(){
+      let nuevoPuntaje = {...this.formData}
+      let puntajeUsuarioPelicula = {
+         value: nuevoPuntaje.puntaje,
+         idMovie: this.index + 1,
+         idUser: this.getUsuarioActual.id
+      }
+      console.log(puntajeUsuarioPelicula)
+      try {
+        await this.axios.post(this.urlPuntuar, puntajeUsuarioPelicula, {'content-type':'application/json'} )
+      }
+      catch(error) {
+        console.error('Error en postScoreUser', error.message)
+      }
+      this.puntajeEnviado = true
+      this.actualizarPeliculas()
+      this.formData = this.getInitialData()
+      this.formstate._reset()
+    }
   },
   computed: {},
 };
